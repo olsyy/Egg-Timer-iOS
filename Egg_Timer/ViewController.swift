dc0ffe7cc1e10aa86f27d073eff9b2e2290b43b4
Olesia Kuklina <o.a.kuklina@yandex.ru>
2023-04-08 09:36:46 -0700

    Initial Commit

---
+++ Egg_Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg_Timer
+//
+//  Created by Lesya Kotty on 08.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
